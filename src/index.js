5de34a84e0bb96df730bc9ea1eef633a60d7352c
Hanthia <nenablanco24@gmail.com>
2020-10-14 09:31:16 +0200

    Add: add polyfills to proyect

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Polyfills
+import '@babel/polyfill';
 
 ReactDOM.render(
   <main>
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'whatwg-fetch',
+    './src/index.js'
+  ],
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: "/",
